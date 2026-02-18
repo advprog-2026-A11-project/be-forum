bd1ef6c4ba4337c5d5ca2c8b4b2372726d54b2bf
alvinchristian <alvch6@gmail.com>
2026-02-18 14:37:17 +0700

    init

---
+++ be-forum/src/main/java/id/ac/ui/cs/advprog/beforum/BeForumApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.beforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeForumApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeForumApplication.class, args);
+  }
+
+}
---
+++ be-forum/src/test/java/id/ac/ui/cs/advprog/beforum/BeForumApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.beforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeForumApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
