f1cff6fe9a60b7402867c7cbee514424dd9be340
Cyaside <tristan.rasheedsatria@gmail.com>
2026-02-19 23:23:57 +0700

    chore : sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 plugins {
     java
+    id("jacoco")
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.1.0.6387"
 }
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
-description = "be-forum"
+description = "auth"
 
 java {
     toolchain {
@@ -24,24 +26,30 @@ repositories {
     mavenCentral()
 }
 
+sonarqube {
+        properties {
+                property("sonar.projectKey", "advprog-2026-A11-project_be-auth")
+                property("sonar.organization", "adpro-a-kelompok-11")
+        }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("org.postgresql:postgresql")
-
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<JacocoReport> {
+    reports {
+        xml.required.set(true)
+    }
 }
